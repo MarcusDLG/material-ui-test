f8beae33b448b9d3e5f3debfb19096504ae41607
MarcusDLG <delag010@gmail.com>
2020-05-20 16:37:29 -0500

    more styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
 import CardActions from '@material-ui/core/CardActions'
+import CardHeader from '@material-ui/core/CardHeader'
 const App = () => {
   return (
     <>
       <Card>
+        <CardHeader className="header">
+          <h1>Mural name</h1>
+          <h2>artist name</h2>
+        </CardHeader>
         <CardMedia
           className="sample"
           image="https://images.unsplash.com/photo-1589691994207-a9e2d0f143d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3072&q=80"
