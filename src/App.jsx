c8cde111f7e39d8603ce6d0501941a3c6347267a
MarcusDLG <delag010@gmail.com>
2020-05-20 16:31:45 -0500

    sample working

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
-
+import Card from '@material-ui/core/Card'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
+import CardActions from '@material-ui/core/CardActions'
 const App = () => {
-  return <></>
+  return (
+    <>
+      <Card>
+        <CardMedia
+          className="sample"
+          image="https://images.unsplash.com/photo-1589691994207-a9e2d0f143d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3072&q=80"
+          title="Paella dish"
+        />
+        <CardContent>
+          <h1>hi</h1>
+          <h2>how are you doing?</h2>
+          <h3>please show this</h3>
+        </CardContent>
+      </Card>
+    </>
+  )
 }
 
 export default App
